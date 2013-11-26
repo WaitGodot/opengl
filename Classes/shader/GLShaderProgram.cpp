0bfbf39cccadc8f22b15c45309deca3fafb29cdc
xiaosirRandy <chen1990214@qq.com>
2013-11-26 21:00:51 +0800

    some files

--- Classes/shader/GLShaderProgram.cpp
+++ Classes/shader/GLShaderProgram.cpp
@@ -7,7 +7,10 @@ namespace glShaderSpace {
 	GLShaderProgram::GLShaderProgram():
 		m_programIdentity(0)
 	{
-
+		for(int i = _Vertex_Start ; i < _Vertex_End ; i ++)
+		{
+			m_glLoction[i] = -1;
+		}
 	}
 
 	
@@ -19,15 +22,21 @@ namespace glShaderSpace {
 		}
 	}
 
+	bool GLShaderProgram::initWithVertexShaderByFile( const char* vertexFile,const char* fragmentFile )
+	{
+
+		return false;
+	}
+
 	bool GLShaderProgram::initWithVertexShaderByteArray( const GLchar* verterArray ,const GLchar* fragArray )
 	{
 		bool result = false;
+		GLint vertexId = 0,fragId =0;
 		do 
 		{
 			BREAK_IF(verterArray == NULL || fragArray == NULL);
 			m_programIdentity = glCreateProgram();
 			BREAK_IF_LOG(m_programIdentity == 0,"create program error id = %d",m_programIdentity);
-			GLint vertexId,fragId;
 			BREAK_IF(compile(vertexId,GL_VERTEX_SHADER,verterArray));
 			BREAK_IF(compile(fragId,GL_FRAGMENT_SHADER,fragArray));
 
@@ -35,8 +44,15 @@ namespace glShaderSpace {
 			glAttachShader(m_programIdentity,fragId);
 			BREAK_IF(link());
 
+			m_glLoction[_Vertex_Position] = glGetAttribLocation(m_programIdentity,AttributePosition);
+			m_glLoction[_Vertex_Color]    = glGetAttribLocation(m_programIdentity,AttributeColor);
+			m_glLoction[_Vertex_texCoords]= glGetAttribLocation(m_programIdentity,AttributeTexCoords);
+
 			result = true;
 		} while (0);
+
+		glDeleteShader(vertexId);
+		glDeleteShader(fragId);
 
 		return result;
 	}
@@ -93,6 +109,13 @@ namespace glShaderSpace {
 	{
 		return glGetUniformLocation(m_programIdentity,(const GLchar*)(uniformName));
 	}
+
+	GLint GLShaderProgram::getVertexAttLoction( VertexAttLoction attribute )
+	{
+		_ASSERT( attribute >= 0 && attribute <= _Vertxt_Count;"getVertexAttLoction error!");
+		return m_glLoction[attribute];
+	}
+
 
 
 
--- Classes/shader/GLShaderProgram.h
+++ Classes/shader/GLShaderProgram.h
@@ -6,18 +6,38 @@
 
 namespace glShaderSpace{
 
-	
+
+#define AttributePosition "a_position"
+#define AttributeColor "a_color"
+#define AttributeTexCoords "a_texCoords"
+
+	enum VertexAttLoction{
+		_Vertex_Position,
+		_Vertex_Color,
+		_Vertex_texCoords,
+
+		_Vertxt_Count,//max :0~15
+		_Vertex_Start = _Vertex_Position,
+		_Vertex_End = _Vertxt_Count,
+	};
+
+
 	class GLShaderProgram 
 	{
 		GLuint m_programIdentity;
+		GLint m_glLoction[_Vertxt_Count];
 
 	public:
 		GLShaderProgram();
 		~GLShaderProgram();
 
+		bool initWithVertexShaderByFile(const char* vertexFile,const char* fragmentFile);
 		bool initWithVertexShaderByteArray(const GLchar* verterArray ,const GLchar* fragArray);
 		GLint getAttribLocation(const char* attribName);
 		GLint getUniformLocation(const char* uniformName);
+		
+		GLint getVertexAttLoction(VertexAttLoction attribute);
+
 	private:
 		bool compile(GLint& shaderId,GLenum type , const GLchar* array);
 		bool link();
